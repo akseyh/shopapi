8a7f41edbae5dadab9121512db1459df51b1875c
Şems Yılmaz <akgulmuhammed@live.com>
2019-11-28 15:55:34 +0300

    initial commit

---
+++ routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const shoppingProductInfo = require('shopping-product-info');
+
+app.get('/', (req, res) => {
+    res.sendFile( __dirname + "/templates/" + "index.html" );
+})
+
+app.get('/api', async (req, res) => {
+    let data = 
+        req.query.origin == 'hepsiburada'
+        ? await shoppingProductInfo('https://www.hepsiburada.com/' + req.query.pathname):
+        req.query.origin == 'hepsiburada'
+        ? await shoppingProductInfo('https://www.trendyol.com/' + req.query.pathname):
+        req.query.origin == 'hepsiburada'
+        ? await shoppingProductInfo('https://www.gittigidiyor.com/' + req.query.pathname):
+        undefined;
+
+    res.send(data);
+})
+
+app.listen(port, () => console.log(`We are live on ${port}!`))
